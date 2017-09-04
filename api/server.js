bcf64dd5034e3f4825161e1ab830e5d24ef8fe3f
Simon CHEVALLIER <schevall@e1r9p14.42.fr>
2017-09-04 16:04:00 +0200

    Final prod reached, fixing npm install prob 2

--- api/server.js
+++ api/server.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import http from 'http';
-import cookieParser from 'cookie-parser';
 import bodyParser from 'body-parser';
 import morgan from 'morgan';
 import multer from 'multer';
@@ -29,7 +28,6 @@ io.on('connection', socket(users));
 Mongo.connect();
 
 app.use(morgan('dev'));
-app.use(cookieParser());
 app.use(bodyParser.urlencoded({ limit: '5mb', extended: true }));
 app.use(bodyParser.json({ limit: '5mb' }));
 app.use('/static', express.static(`${__dirname}/uploads`));
